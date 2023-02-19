8395a1ee5343d932703ee81a4b6f5f9e0b85dee4
Benedek Harsányi <benedek.harsanyi@macbookpro-cdhcq7hvwf.home>
2023-02-19 12:00:33 +0100

    create separate functions for scraping articles and pages

--- telex-nlp/pipeline/scrape/scraping.py
+++ telex-nlp/pipeline/scrape/scraping.py
@@ -12,7 +12,7 @@ from bs4 import BeautifulSoup as BS
 from params import MAX_PAGES
 
 
-def get_soup(url) -> None:
+def get_soup(url) -> BS:
     """Get soup from the web."""
     response = requests.request("GET", url)
     soup = BS(response.content, "html.parser")
@@ -44,35 +44,52 @@ def parse_author(raw):
     return raw.split("\n")[4].lstrip()
 
 
+def scrape_article_metadata(article, data):
+    href = article.find("a", href=True)["href"]
+    if not href[1:8] == "english":  # for now skipping english articles
+        data["href"].append(href)
+        data["language"].append("hu")
+        raw = article.find(class_="article_date").text
+        try:
+            data["author"].append(parse_author(raw))
+        except:
+            data["author"].append("")
+        try:
+            data["date"].append(parse_date(raw))
+        except:
+            data["date"].append(raw)
+    data["lead"].append(article.find("p", class_="list__item__lead hasHighlight").text)
+
+    text = scrape_text(href)
+    data["text"].append(text)
+
+
+def scrape_text(href):
+    soup2 = get_soup("https://telex.hu" + href)
+    article = soup2.find("div", {"class": "article-html-content"})
+    return article.text
+
+
+def scrape_archive_page(page: int) -> dict:
+    url = f"https://telex.hu/archivum?oldal={page}"
+    data = {"date": [], "author": [], "lead": [], "href": [], "language": [], "text": []}
+
+    soup = get_soup(url)
+    list_group = soup.find_all("div", {"class": "list__item__info"})
+    for i, article in enumerate(list_group):
+        scrape_article_metadata(article, data)
+    return data
+
+
+def save_json(data, output_folder: Path, page: int) -> None:
+    with open(output_folder / f"raw_archive_{page}.json", "w") as outfile:
+        json.dump(data, outfile, default=str)
+
+
 def main(output_folder: Path = typer.Option(...)) -> None:
     for page in tqdm.tqdm(range(MAX_PAGES)):
-        url = "https://telex.hu/archivum?oldal={i}"
-        soup = get_soup(url)
-        list_group = soup.find_all("div", {"class": "list__item__info"})
-        data = {"date": [], "author": [], "lead": [], "href": [], "language": [], "text": []}
-        for i, article in enumerate(list_group):
-            href = article.find("a", href=True)["href"]
-            if not href[1:8] == "english":  # for now skipping english articles
-                data["href"].append(href)
-                data["language"].append("hu")
-                raw = article.find(class_="article_date").text
-                try:
-                    data["author"].append(parse_author(raw))
-                except:
-                    data["author"].append("")
-                try:
-                    data["date"].append(parse_date(raw))
-                except:
-                    data["date"].append(raw)
-            data["lead"].append(article.find("p", class_="list__item__lead hasHighlight").text)
-
-        soup2 = get_soup("https://telex.hu" + href)
-        article = soup2.find_all("div", {"class": "article-html-content"})[0]
-        data["text"].append(article.text)
-
-        # Using a JSON string
-        with open(output_folder / f"raw_archive_{page}.json", "w") as outfile:
-            json.dump(data, outfile, default=str)
+        archive_page = scrape_archive_page(page)
+        save_json(archive_page, output_folder, page)
 
 
 if __name__ == "__main__":
