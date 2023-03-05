2068c53a12c783532e79167071b2a65fa189c176
hbenedek <hben.0204@gmail.com>
2023-03-05 21:47:45 +0100

    fix evaluation

--- telex/pipeline/evaluate/evaluation.py
+++ telex/pipeline/evaluate/evaluation.py
@@ -10,9 +10,10 @@ import typer
 from torch.functional import F
 from torch.utils.data import DataLoader
 
-from params import VOCAB, TrainingParams
+from params import MODEL, VOCAB, BigramParams, TrainingParams, TransformerParams
 from telex.models.base_model import BaseModel
 from telex.models.bigram import BigramLM
+from telex.models.transformer import GPT
 from telex.utils.dataset import CharDataset
 
 
@@ -26,9 +27,12 @@ def perplexity(model: BaseModel, loader: DataLoader, subset: Optional[int] = Non
         logits, log_probs = model(idx, targets)
         targets = targets.view(-1)
         log_probs = F.log_softmax(logits, dim=-1)
+        B, T, C = log_probs.size()
+        log_probs = log_probs.view(B * T, C)
         log_likelihood = torch.sum(log_probs.gather(1, targets.unsqueeze(-1)).squeeze(-1))
         total_log_likelihood += log_likelihood.item()
         total_num_chars += targets.numel()
+        print(k, log_likelihood.item())
         if subset is not None:
             if k > subset:
                 break
@@ -56,12 +60,29 @@ def plot_learning_curve(model: BaseModel, output_folder: Path) -> None:
 def main(model_file: Path = typer.Option(...), input_file: Path = typer.Option(...)) -> None:
     dataset = CharDataset(input_file, VOCAB, TrainingParams.BLOCK_SIZE, type_="test", split=TrainingParams.SPLIT)
     test_loader = DataLoader(dataset, batch_size=TrainingParams.BATCH_SIZE, shuffle=True)
-    model_kwargs = {"vocab": VOCAB, "block_size": TrainingParams.BLOCK_SIZE}
-    model = BigramLM.load_for_prediction(model_file, model_kwargs)
-    # model = BigramLM.create_for_training(model_file, model_kwargs)
+    if MODEL == "bigram":
+        model_kwargs = {"vocab": VOCAB, "block_size": BigramParams.BLOCK_SIZE}
+        model = BigramLM.load_for_prediction(model_file, model_kwargs)
+    elif MODEL == "transformer":
+        model_kwargs = {
+            "vocab": VOCAB,
+            "block_size": TransformerParams.BLOCK_SIZE,
+            "num_layers": TransformerParams.NUM_LAYERS,
+            "heads": TransformerParams.HEADS,
+            "d_model": TransformerParams.D_MODEL,
+            "d_ff": TransformerParams.D_FF,
+            "dropout": TransformerParams.DROPOUT,
+        }
+        model = GPT.load_for_prediction(model_file, model_kwargs)
     plot_learning_curve(model, model_file.parent)
     p = perplexity(model, test_loader, subset=1000)
     print(f"Perplexity: {p}")
+
+    # generate random text
+    idx = torch.tensor([[0]])
+    outs = model.generate(idx, 500)[0]
+    outs = dataset.decode(outs)
+    print(outs)
 
 
 if __name__ == "__main__":
